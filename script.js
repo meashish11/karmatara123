4d56be65b75dfff9ded40568ae6e88b9422480dd
Ashish Thapa <mekaji181@gmail.com>
2024-02-26 22:14:43 +0545

    first draft

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSidebar() {
+    const siderbar = document.querySelector('.sidebar')
+    siderbar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const siderbar = document.querySelector('.sidebar')
+    siderbar.style.display = 'none'
+}
